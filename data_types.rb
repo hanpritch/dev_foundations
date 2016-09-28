a5fed2d21e7fef9788354ff36e71a9d3587dcf35
Hannah Pritchett <hannah@stripe.com>
2016-09-27 19:00:59 -0700

    adding ruby file

---
+++ data_types.rb
@@ -0,0 +1,19 @@
+string = "'This is a string'"
+integer = 1
+boolean = true 
+array = [1, 2, 3]
+hash = {"color"=>"red"}
+
+
+puts "Welcome to our Ruby tutorial. First, we'll talk about data types."
+
+puts "A string is a list of characters, like #{string}."
+
+puts "An integer is a whole number, like #{integer}."
+
+puts "A boolean is a true/false value, like #{boolean}."
+
+puts "An array is a list, like #{array}."
+
+puts "A hash is a key/value pair, like #{hash}."
+
